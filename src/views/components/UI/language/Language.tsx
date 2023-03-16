3b475b907476b78f6e225a1516ac10eefc27aded
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-16 17:23:42 +0100

    Add Language component and index file to manage language selector.

---
+++ src/views/components/UI/language/Language.tsx
@@ -0,0 +1,25 @@
+import { useTranslation } from "react-i18next";
+
+const lngs = [
+  { code: "en", native: "English" },
+  { code: "es", native: "Spanish" },
+];
+
+const Language = () => {
+  const { i18n } = useTranslation();
+
+  const handleTrans = (code: string) => {
+    i18n.changeLanguage(code);
+  };
+
+  return (
+    <div style={{ padding: '50px' }}>
+      {lngs.map((lng, i) => {
+        const { code, native } = lng;
+        return <button key={i} onClick={() => handleTrans(code)}>{native}</button>;
+      })}
+    </div>
+  )
+}
+
+export default Language;
