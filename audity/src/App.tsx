b9a1884d4f5ee2055133710fb36b71b7dd348da3
lucydct <tenalucia0@gmail.com>
2023-03-16 16:38:27 +0100

    feat(createProyect):install Vite, TS, Sass

---
+++ audity/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1>hola mundo</h1>
+  )
+}
+
+export default App
