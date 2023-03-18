e43c6809d75c95851f5ebd30999ad2853e020ca5
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-18 14:37:56 +0100

    Add Auth0 context to PageTopBar Component.

--- src/views/components/component/pageTopbar/PageTopBar.tsx
+++ src/views/components/component/pageTopbar/PageTopBar.tsx
@@ -1,20 +1,22 @@
 import { useTranslation } from 'react-i18next';
 import './PageTopBar.scss'
 import { BsSearch } from 'react-icons/bs'
+import { useAuth0 } from '@auth0/auth0-react';
 
 const PageTopBar = () => {
   const { t } = useTranslation();
+  const { user } = useAuth0();
 
   return (
     <header>
-      
+
       <div className='topbar-search'>
         <BsSearch color='#52525d' />
-        <input type="text" className='search-topbar' placeholder={t("search_placeholder") || ""}/>
+        <input type="text" className='search-topbar' placeholder={t("search_placeholder") || ""} />
       </div>
-      
+
       <button className='topbar-profile'>
-        <img src='https://e-cdns-images.dzcdn.net/images/user//32x32-000000-80-0-0.jpg' alt="avatar" />
+        <img src={user?.picture ? user.picture : 'https://e-cdns-images.dzcdn.net/images/user//32x32-000000-80-0-0.jpg'} alt="avatar" />
       </button>
     </header>
   )
