ef68717788974d0be101d648f9b058f495615826
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-18 14:38:40 +0100

    Add Login/Logout buttons to HomeUserComponent.tsx

--- src/views/components/user/homeUserComponent/HomeUserComponent.tsx
+++ src/views/components/user/homeUserComponent/HomeUserComponent.tsx
@@ -1,11 +1,15 @@
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react'
 import Language from '../../UI/language/Language';
 import Theme from '../../UI/theme/Theme';
 import './homeUserComponent.scss';
 
 const HomeUserComponent = () => {
   const { t } = useTranslation();
+  const { loginWithRedirect, logout, isAuthenticated, isLoading } = useAuth0()
+
+  if (isLoading) return <h1>Loading</h1>
 
   return (
     <div className='home-user-component'>
@@ -13,6 +17,16 @@ const HomeUserComponent = () => {
       <Language />
       <h1>{t("title_user_home")}</h1>
       <Link to={'/music'}>Music</Link>
+      <br />
+      {isAuthenticated ? (
+        <button type='button' className='nes-btn is-primary login-button' onClick={() => logout()}>
+          Logout
+        </button>
+      ) : (
+        <button type='button' className='nes-btn is-primary login-button' onClick={() => loginWithRedirect()}>
+          Login
+        </button>
+      )}
     </div>
   )
 }
