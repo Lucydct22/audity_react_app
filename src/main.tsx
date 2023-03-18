88114a7b83487db21f5ede081a023853a2b6efcd
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-18 14:36:32 +0100

    Add Auth0 authentication provider to App component.

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.scss'
 import './i18n'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Auth0Provider
+    domain={import.meta.env.VITE_APP_AUTH0_DOMAIN as string}
+    clientId={import.meta.env.VITE_APP_AUTH0_CLIENT_ID as string}
+    authorizationParams={{ redirect_uri: window.location.origin + '/music' }}
+  >
+      <App />
+  </Auth0Provider>
 )
