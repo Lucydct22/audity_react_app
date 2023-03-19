51307aeec744df0840061d86923e571a8086cd32
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-19 20:30:49 +0100

    Add HelmetSEO component and index file to views/utils

---
+++ src/views/utils/HelmetSEO/HelmetSEO.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+
+type Props = {
+  title: string,
+  description?: string,
+  children: React.ReactNode
+}
+
+export default function HelmetSEO(props: Props) {
+  const { title, description, children } = props;
+
+  return (
+    <HelmetProvider>
+      <Helmet>
+        <title>{title}</title>
+        <meta name="description" content={description}></meta>
+      </Helmet>
+      {children}
+    </HelmetProvider>
+  );
+}
