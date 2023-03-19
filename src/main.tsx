b405474eb21fbac107228e9b5c93a74effe47cf3
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-19 21:25:30 +0100

    Update Auth0 credentials to use different client IDs in development and production.

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Auth0Provider } from '@auth0/auth0-react';
+import { isLocalhost } from './utils/isLocalhost';
 import './utils/i18n';
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Auth0Provider
-    domain={import.meta.env.VITE_APP_AUTH0_DOMAIN as string}
-    clientId={import.meta.env.VITE_APP_AUTH0_CLIENT_ID as string}
+    domain={import.meta.env.VITE_AUTH0_DOMAIN as string}
+    clientId={
+      isLocalhost
+        ? import.meta.env.VITE_DEVELOPMENT_AUTH0_CLIENT_ID as string
+        : import.meta.env.VITE_PRODUCTION_AUTH0_CLIENT_ID as string
+    }
     authorizationParams={{ redirect_uri: window.location.origin + '/music' }}
   >
     <App />
