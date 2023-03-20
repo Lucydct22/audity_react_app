8aac130c80d3955993b0c009148b75fdf7e232af
Joe Alt <joejoyalt@gmail.com>
2023-03-20 17:06:58 +0100

    feat(popup): user action added

--- src/views/components/basic/testComponent/TestComponent.tsx
+++ src/views/components/basic/testComponent/TestComponent.tsx
@@ -1,33 +1,19 @@
 import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react'
 import Language from '../../../UI/language/Language';
-import Theme from '../../../UI/theme/Theme';
 import './testComponent.scss';
 import Spinner from '../../../UI/spinner/Spinner';
 
 const TestComponent = () => {
   const { t } = useTranslation();
-  const { user, loginWithRedirect, logout, isAuthenticated, isLoading } = useAuth0()
+  const { user, isAuthenticated, isLoading } = useAuth0()
 
   if (isLoading) return <Spinner />
 
   return (
     <div className='home-user-component'>
-      <Theme />
       <Language />
       {isAuthenticated && <h1>{t("title_user_home")} {user?.name}</h1>}
-      <Link to={'/music'}>Music</Link>
-      <br />
-      {isAuthenticated ? (
-        <button type='button' className='nes-btn is-primary login-button' onClick={() => logout()}>
-          Logout
-        </button>
-      ) : (
-        <button type='button' className='nes-btn is-primary login-button' onClick={() => loginWithRedirect()}>
-          Login
-        </button>
-      )}
     </div>
   )
 }
--- src/views/components/basic/topBarBComponent/TopBarBComponent.tsx
+++ src/views/components/basic/topBarBComponent/TopBarBComponent.tsx
@@ -1,11 +1,18 @@
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import './topBarBComponent.scss';
-import { BsSearch } from 'react-icons/bs';
 import { useAuth0 } from '@auth0/auth0-react';
+import './topBarBComponent.scss';
+import Language from '../../../UI/language/Language';
+import Theme from '../../../UI/theme/Theme';
+import { BsSearch } from 'react-icons/bs';
+import { GoTriangleUp } from 'react-icons/go';
+import { FiChevronRight } from 'react-icons/fi';
+
 
 const TopBarBComponent = () => {
   const { t } = useTranslation();
-  const { user } = useAuth0();
+  const { user, loginWithRedirect, logout, isAuthenticated } = useAuth0();
+  const [popperIsActive, setPopperIsActive] = useState(false);
 
   return (
     <header className='page-topbar'>
@@ -14,9 +21,40 @@ const TopBarBComponent = () => {
         <input type="text" className='page-topbar-search__input' placeholder={t("search_placeholder") || ""} />
       </div>
 
-      <button className='page-topbar-profile'>
-        <img src={user?.picture ? user.picture : 'https://e-cdns-images.dzcdn.net/images/user//32x32-000000-80-0-0.jpg'} alt="avatar" />
-      </button>
+      <div className='page-topbar-action'>
+        <button className='page-topbar-action__profile' onClick={() => setPopperIsActive(!popperIsActive)}>
+          <img src={user?.picture ? user.picture : 'https://e-cdns-images.dzcdn.net/images/user//32x32-000000-80-0-0.jpg'} alt="avatar" />
+        </button>
+        {popperIsActive ?
+          <div className='page-topbar-action__popper'>
+            <div className='page-topbar-action__popper--wrapper'>
+              <div className='page-topbar-action__popper--wrapper__icon'>
+                <GoTriangleUp />
+              </div>
+              <div className='page-topbar-action__popper--wrapper__content cursor-pointer'>
+                <span>Account Settings</span>
+                <FiChevronRight size='1.4rem' />
+              </div>
+              <div className='page-topbar-action__popper--wrapper__content'>
+                <span className='cursor-default'>System theme</span>
+                <Theme />
+              </div>
+              <div className='page-topbar-action__popper--wrapper__content cursor-pointer'>
+                {isAuthenticated ? (
+                  <span onClick={() => logout()}>
+                    Log out
+                  </span>
+                ) : (
+                  <span onClick={() => loginWithRedirect()}>
+                    Login
+                  </span>
+                )}
+              </div>
+            </div>
+          </div>
+          : null
+        }
+      </div>
     </header>
   )
 }
