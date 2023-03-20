57b44d66b69b24fcf09650efc6d68345e6a30076
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-20 09:53:26 +0100

    Add SEO tags to Error page.

--- src/views/pages/errors/Error.tsx
+++ src/views/pages/errors/Error.tsx
@@ -1,5 +1,12 @@
+import HelmetSEO from "../../utils/HelmetSEO";
+
 export default function Error() {
   return (
-    <div>Error</div>
+    <HelmetSEO
+      title='Error 404 | Audity'
+      description='Audity Error Page'
+    >
+      <div>Error 404</div>
+    </HelmetSEO>
   )
 }
