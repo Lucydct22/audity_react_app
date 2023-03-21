f0cf0278b8164310170a7153945b8b44795fdc08
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-21 17:25:49 +0100

    Add likeDislikeTrackApi to ApiTestPage and update associated logic

--- src/views/pages/apiTestPage/ApiTestPage.tsx
+++ src/views/pages/apiTestPage/ApiTestPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { getTracksApi, getTrackByIdApi, deleteTrackByIdApi, updateTrackApi, addTrackApi, } from '../../../api/music/tracks';
+import { getTracksApi, getTrackByIdApi, deleteTrackByIdApi, updateTrackApi, addTrackApi, likeDislikeTrackApi } from '../../../api/music/tracks';
 import { getAlbumsApi, getAlbumsByIdApi, deleteAlbumsByIdApi, updateAlbumApi, addAlbumApi } from '../../../api/music/albums';
 import { getArtistApi, getArtistByIdApi, deleteArtistByIdApi, updateArtistApi, addArtistApi } from '../../../api/music/artists';
 import { getPlaylistApi, getPlaylistByIdApi, deletePlaylistByIdApi, updatePlaylistApi, addPlaylistApi } from '../../../api/music/playlists';
@@ -7,23 +7,36 @@ import { getGenresApi, getGenresByIdApi, deleteGenresByIdApi, updateGenresApi, a
 import { Albums, Track, Genres, Playlist, Artist } from '../../../interfaces/music';
 export default function ApiTestPage() {
 	// TRACKS
-	getTracksApi().then((data: Track) => { console.log(data) })
-	getTrackByIdApi('1').then((data: Track) => { console.log(data) })
+	// getTracksApi().then((data: Track) => { console.log(data) })
+	// getTrackByIdApi('1').then((data: Track) => { console.log(data) })
 	// deleteTrackByIdApi('1').then((data: Track) => { console.log(data) })
-	// const updateDataTrack = { name: "Better of alone" }
+	// const updateDataTrack = {
+	// 	name: "Better of alone v2",
+	// 	artist: "RXBYN",
+	// 	url: "https://res.cloudinary.com/dmkdsujzh/video/upload/v1644583924/tracks-dev/Rxbyn_-_better_off_alone_fvhwp8.mp3",
+	// 	thumbnail: "https://res.cloudinary.com/dmkdsujzh/image/upload/v1644583757/tracks-thumbnails-dev/better_off_alone_gfmcby.jpg",
+	// 	genre: ["piano"],
+	// 	likes: ["1", "2", "3"],
+	// 	album: "1",
+	// 	createdAt: Date.now(),
+	// 	updatedAt: Date.now()
+	// };
 	// updateTrackApi('1', updateDataTrack).then((data: Track) => { console.log(data) });
 	// const addDataTrack = {
-	// 	id: "1",
 	// 	name: "Better of alone",
 	// 	artist: "RXBYN",
 	// 	url: "https://res.cloudinary.com/dmkdsujzh/video/upload/v1644583924/tracks-dev/Rxbyn_-_better_off_alone_fvhwp8.mp3",
 	// 	thumbnail: "https://res.cloudinary.com/dmkdsujzh/image/upload/v1644583757/tracks-thumbnails-dev/better_off_alone_gfmcby.jpg",
-	// 	genre: "piano",
+	// 	genre: ["piano"],
 	// 	likes: ["1", "2", "3"],
 	// 	album: "1",
-	// 	createdAt: "2023-03-14T09:17:23.739+00:00",
-	// 	updatedAt: "2023-03-14T09:17:23.739+00:00"
-	// }
+	// 	createdAt: Date.now(),
+	// 	updatedAt: Date.now()
+	// };
+	// const updateDataTrack = { likeuserId: "1" }
+	// likeDislikeTrackApi('1', updateDataTrack).then((data: any) => { console.log(data.likes) });
+
+	// addTrackApi(addDataTrack).then((data: Track) => { console.log(data) });
 	// likeTrackApi('1', '4').then((data: Track) => { console.log(data) });
 	// unlikeTrackApi('1', '2').then((data: Track) => { console.log(data) });
 	// likeTrackApi('1', '4').then((data: Track) => { console.log(data) });
@@ -33,7 +46,6 @@ export default function ApiTestPage() {
 	// getAlbumsApi().then((data: Albums) => { console.log(data) })
 	// getAlbumsByIdApi('1').then((data: Albums) => { console.log(data) })
 	// deleteAlbumsByIdApi('1').then((data: Albums) => { console.log(data) })
-	// const updateDataAlbum = { name: "Flyga" }
 	// updateAlbumApi('1', updateDataAlbum).then((data: Albums) => { console.log(data) });
 	// const addDataAlbum = {
 	// 	id: "1",
@@ -41,28 +53,27 @@ export default function ApiTestPage() {
 	// 	imageUrl: "https://res.cloudinary.com/dmkdsujzh/video/upload/v1644583924/tracks-dev/Social_Square_-_Tantalizing_Youth_eykq87.mp3",
 	// 	artist: "Social Square",
 	// 	genres: ["Rock"],
-	// 	createdAt: "2023-03-14T09:17:23.739+00:00",
-	// 	updatedAt: "2023-03-14T09:17:23.739+00:00"
+	// 	createdAt: Date.now(),
+	// 	updatedAt: Date.now()
 
 	// }
-	//addAlbumApi(addDataAlbum).then((data: Albums) => { console.log(data) });
+	// addAlbumApi(addDataAlbum).then((data: Albums) => { console.log(data) });
 
 
 	//ARTISTS
 	// getArtistApi().then((data: Artist) => { console.log(data) })
 	// getArtistByIdApi('1').then((data: Artist) => { console.log(data) })
 	// deleteArtistByIdApi('1').then((data: Artist) => { console.log(data) })
-	// const updateDataArtist = { name: "Kim Cesarion" }
 	// updateArtistApi('1', updateDataArtist).then((data: Artist) => { console.log(data) });
 	// const addDataArtist = {
 	// 	id: "1",
 	// 	name: "Kim Cesarion",
 	// 	genres: ["blues", "rock", "pop", "soul"],
 	// 	photoUrl: "https://i.scdn.co/image/ab6761610000e5eb842fae710a21f648e26dd910",
-	// 	createdAt: "2023-03-14T09:17:23.739+00:00",
-	// 	updatedAt: "2023-03-14T09:17:23.739+00:00"
+	// 	createdAt: Date.now(),
+	// 	updatedAt: Date.now()
 	// }
-	//addArtistApi(addDataArtist).then((data: Artist) => { console.log(data) });
+	// addArtistApi(addDataArtist).then((data: Artist) => { console.log(data) });
 
 
 
@@ -70,7 +81,6 @@ export default function ApiTestPage() {
 	// getPlaylistApi().then((data: Playlist) => { console.log(data) })
 	// getPlaylistByIdApi('1').then((data: Playlist) => { console.log(data) })
 	// deletePlaylistByIdApi('1').then((data: Playlist) => { console.log(data) })
-	// const updateDataPlaylist = { name: "Road trip! All the masters I need!" }
 	// updatePlaylistApi('1', updateDataPlaylist).then((data: Playlist) => { console.log(data) });
 	// const addDataPlaylist = {
 	// 	id: "1",
@@ -79,25 +89,24 @@ export default function ApiTestPage() {
 	// 	description: "Get your mic on with this beats. You are going to sing all the way down",
 	// 	publicAccessible: false,
 	// 	musicList: ["1", "2", "3", "4", "5"],
-	// 	createdAt: "2023-03-14T09:17:23.739+00:00",
-	// 	updatedAt: "2023-03-14T09:17:23.739+00:00"
+	// 	createdAt: Date.now(),
+	// 	updatedAt: Date.now()
 	// }
-	//addPlaylistApi(addDataPlaylist).then((data: Playlist) => { console.log(data) });
+	// addPlaylistApi(addDataPlaylist).then((data: Playlist) => { console.log(data) });
 
 
 	//GENRES
 	// getGenresApi().then((data: Genres) => { console.log(data) })
 	// getGenresByIdApi('1').then((data: Genres) => { console.log(data) })
 	// deleteGenresByIdApi('1').then((data: Genres) => { console.log(data) })
-	// const updateDataGenres = { name: "Kim Cesarion" }
 	// updateGenresApi('1', updateDataGenres).then((data: Genres) => { console.log(data) });
 	// const addDataGenres = {
 	// 	id: "1",
 	// 	name: "blues",
-	// 	createdAt: "2023-03-14T09:17:23.739+00:00",
-	// 	updatedAt: "2023-03-14T09:17:23.739+00:00"
+	// 	createdAt: Date.now(),
+	// 	updatedAt: Date.now()
 	// }
-	//addGenresApi(addDataGenres).then((data:Genres) => { console.log(data) });
+	// addGenresApi(addDataGenres).then((data:Genres) => { console.log(data) });
 
 
 
