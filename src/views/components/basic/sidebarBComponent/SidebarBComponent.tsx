5ffaba5781404642c214062b690f089ea11b0396
Javier Pascual <pascualjpt@gmail.com>
2023-03-21 09:31:56 +0100

    Sidebar style done, req. change logo colors.

--- src/views/components/basic/sidebarBComponent/SidebarBComponent.tsx
+++ src/views/components/basic/sidebarBComponent/SidebarBComponent.tsx
@@ -13,7 +13,7 @@ const SidebarBComponent = () => {
 
   return (
 
-    <nav className='page-sidebar'>
+    <nav className='side-bar'>
       <div className='side-bar-header'>
         <div className='side-bar-header__logo'>
           <Link to={'/'}>
@@ -23,27 +23,32 @@ const SidebarBComponent = () => {
 
         <div className='side-bar-header__sections'>
           <NavLink to={'/music'} className='side-bar-header__sections--options'>
-            <FiMusic className='side-bar-header__sections--options__icon'/>
+            <div className='side-bar-header__sections--options__decoration'></div>
+            <FiMusic size={20} className='side-bar-header__sections--options__icon' />
             <p>{t("sidebar_music")}</p>
           </NavLink>
 
           <NavLink to={'/radio'} className='side-bar-header__sections--options'>
-            <RiRadioLine className='side-bar-header__sections--options__icon'/>
+            <div className='side-bar-header__sections--options__decoration'></div>
+            <RiRadioLine size={20} className='side-bar-header__sections--options__icon' />
             <p>{t("sidebar_radio")}</p>
           </NavLink>
 
           <NavLink to={'/browse'} className='side-bar-header__sections--options'>
-            <CgBrowser className='side-bar-header__sections--options__icon'/>
+            <div className='side-bar-header__sections--options__decoration'></div>
+            <CgBrowser size={20} className='side-bar-header__sections--options__icon' />
             <p>{t("sidebar_browser")}</p>
           </NavLink>
 
           <NavLink to={'/favorites'} className='side-bar-header__sections--options'>
-            <AiOutlineHeart className='side-bar-header__sections--options__icon'/>
+            <div className='side-bar-header__sections--options__decoration'></div>
+            <AiOutlineHeart size={20} className='side-bar-header__sections--options__icon' />
             <p>{t("sidebar_favorites")}</p>
           </NavLink>
 
           <NavLink to={'/your-studio'} className='side-bar-header__sections--options'>
-            <BsMusicPlayer className='side-bar-header__sections--options__icon'/>
+            <div className='side-bar-header__sections--options__decoration'></div>
+            <BsMusicPlayer size={20} className='side-bar-header__sections--options__icon' />
             <p>{t("sidebar_yourstudio")}</p>
           </NavLink>
 
