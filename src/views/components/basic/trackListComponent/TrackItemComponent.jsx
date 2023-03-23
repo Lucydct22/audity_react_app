b67f446ad2d9ba5078ce67607469d7a4a6b3c20b
Iuliia Shikhanova <yshihanova@gmail.com>
2023-03-23 16:23:34 +0000

    undate: responsive design tracklist for mobile

--- src/views/components/basic/trackListComponent/TrackItemComponent.jsx
+++ src/views/components/basic/trackListComponent/TrackItemComponent.jsx
@@ -1,21 +1,48 @@
-import './trackListStyle.scss'; 
+import './trackListStyle.scss';
 import { IoAddOutline } from "react-icons/io5";
 import { AiFillHeart } from "react-icons/ai";
 import { AiOutlineDownload } from "react-icons/ai";
+import { responsiveBreak } from "../../../../utils/componentsConstants";
+import useWindowSizeReport from "../../../../hooks/useWindowSizeReport";
+import { IoEllipsisVerticalSharp } from "react-icons/io5";
+
 
 const TrackItemComponent = ({ id, name, artist, thumbnail, likes, album, time }) => {
 
+  const windowWidth = useWindowSizeReport()
+
   return (
     <>
-      <tr key={id} className="track-list-item">
-        <td className='track-list-item__td-track'><img src={thumbnail} alt={name} />{name}</td>
-        <td className='track-list-item__td-icon'><IoAddOutline className='track-list-item__td-icon-item'/></td>
-        <td className='track-list-item__td-icon'><AiOutlineDownload className='track-list-item__td-icon-item'/></td>
-        <td>{artist}</td>
-        <td>{album}</td>
-        <td>{time}</td>
-        <td className='track-list-item__td-icon'><AiFillHeart className='track-list-item__td-icon-heart'/>{likes}</td>
-      </tr>
+      {responsiveBreak < windowWidth ? (
+        <tr key={id} className="track-list-item">
+          <td className='track-list-item__td-track'><img src={thumbnail} alt={name} />{name}</td>
+          <td className='track-list-item__td-icon'><IoAddOutline className='track-list-item__td-icon-item' /></td>
+          <td className='track-list-item__td-icon'><AiOutlineDownload className='track-list-item__td-icon-item' /></td>
+          <td>{artist}</td>
+          <td>{album}</td>
+          <td>{time}</td>
+          <td className='track-list-item__td-icon'><AiFillHeart className='track-list-item__td-icon-heart' />{likes}</td>
+        </tr>
+      ) : (
+        <div key={id} className="track-list-item-mobile">
+          <div className="track-list-item-mobile-div">
+            <img src={thumbnail} alt={name} />
+            <div className="track-list-item-mobile-div__track">
+              <span>{name}</span>
+              <div className="track-list-item-mobile-div__track-album">
+                <span>{artist} - </span>
+                <span>{album}</span>
+              </div>
+            </div>
+          </div>
+          <div className="track-list-item-mobile__icons-mobile">
+            <AiFillHeart className='track-list-item-mobile__icons-mobile__td-icon-heart' />
+            <IoEllipsisVerticalSharp className='track-list-item-mobile__icons-mobile__td-icon-points'/>
+          </div>
+        </div>
+      )
+      }
+
     </>
   )
 }
