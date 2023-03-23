0b1869781d8621ca77c0703a07a7c3f93cca813f
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-23 17:58:57 +0100

    Add album to trackReducer, handle NEXT_TRACK and PREV_TRACK actions in trackReducer

--- src/reducers/track/trackReducer.ts
+++ src/reducers/track/trackReducer.ts
@@ -2,11 +2,12 @@ import * as TrackTypes from './trackTypes';
 
 export default function trackReducer(state: any, action: any) {
 	const { type, payload } = action;
-	
+
 	switch (type) {
 		case TrackTypes.INIT_CURRENT_TRACK:
 			return {
 				currentTrack: payload.currentTrack,
+				album: payload.album,
 				trackData: payload.trackData,
 			}
 
@@ -14,6 +15,7 @@ export default function trackReducer(state: any, action: any) {
 			state.trackData.audio.play();
 			return {
 				currentTrack: state.currentTrack,
+				album: state.album,
 				trackData: { ...state.trackData, isPlaying: true }
 			}
 
@@ -21,17 +23,33 @@ export default function trackReducer(state: any, action: any) {
 			state.trackData.audio.pause();
 			return {
 				currentTrack: state.currentTrack,
+				album: state.album,
 				trackData: { ...state.trackData, isPlaying: false }
 			}
 
 		case TrackTypes.UPDATE_CURRENT_TIME:
 			return {
 				currentTrack: state.currentTrack,
+				album: state.album,
 				trackData: {
 					...state.trackData,
 					currentTime: payload,
 					timeToEnd: state.trackData.duration - payload
 				}
+			}
+
+		case TrackTypes.NEXT_TRACK:
+			return {
+				currentTrack: payload.currentTrack,
+				album: state.album,
+				trackData: payload.trackData
+			}
+
+		case TrackTypes.PREV_TRACK:
+			return {
+				currentTrack: payload.currentTrack,
+				album: state.album,
+				trackData: payload.trackData
 			}
 
 		default:
