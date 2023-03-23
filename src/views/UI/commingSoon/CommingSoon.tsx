f5bd7ce0239704242eaea5ddb76f0f5cf2b34561
Javier Pascual <pascualjpt@gmail.com>
2023-03-23 17:07:53 +0100

    Edit commingSoon as component

--- src/views/UI/commingSoon/CommingSoon.tsx
+++ src/views/UI/commingSoon/CommingSoon.tsx
@@ -1,11 +1,11 @@
-import Gif1 from '../../../assets/img/gif/giphy.gif';
+import Gif1 from '../../../assets/img/gif/giphy1-unscreen.gif';
 import "./commingSoon.scss";
 
 export default function CommingSoon() {
 	return (
 		<div className='comming-soon-component'>
 			<img src={Gif1} alt="gif" />
-			<h1>We are sorry for the inconvenience, we keep working on it ...</h1>
+			<p>We are sorry for the inconvenience, we keep working on it ...</p>
 		</div>
 	)
 }
