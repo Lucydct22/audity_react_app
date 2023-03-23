325102fed19897108cee635ae2901f39c5da6f07
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-23 19:21:49 +0100

    Update trackActions to use getTracksApi and makeArrayOfTrackIds for initializing and cycling current track.

--- src/reducers/track/trackActions.ts
+++ src/reducers/track/trackActions.ts
@@ -1,15 +1,16 @@
-import { getAlbumsByIdApi } from "../../api/music/albums";
-import { getTrackByIdApi } from "../../api/music/tracks";
+import { getTracksApi, getTrackByIdApi } from "../../api/music/tracks";
 import { getDuration } from "../../utils/tracks/getDuration";
 import { initAudio } from "../../utils/tracks/initAudio";
+import makeArrayOfTrackIds from "../../utils/tracks/makeArrayOfTrackIds";
 import { tracksCycle } from "../../utils/tracks/tracksCycle";
 import * as TrackTypes from './trackTypes'
 
 export const initCurrentTrackAction = function (dispatch: any) {
-	getAlbumsByIdApi('1').then(albumResponse => {
-		const random = Math.floor(Math.random() * albumResponse.tracks.length);
+	getTracksApi().then(tracksResponse => {
+		const tracksList = makeArrayOfTrackIds(tracksResponse);
+		const random = Math.floor(Math.random() * tracksList.length);
 
-		getTrackByIdApi(albumResponse.tracks[random]).then(async res => {
+		getTrackByIdApi(random.toString()).then(async res => {
 			if (res) {
 				const audio: any = initAudio(res);
 				const duration: any = await getDuration(res.url);
@@ -18,7 +19,7 @@ export const initCurrentTrackAction = function (dispatch: any) {
 					type: TrackTypes.INIT_CURRENT_TRACK,
 					payload: {
 						currentTrack: res,
-						album: albumResponse.tracks,
+						tracksList: tracksList,
 						trackData: {
 							url: res.url,
 							audio: audio,
@@ -35,54 +36,54 @@ export const initCurrentTrackAction = function (dispatch: any) {
 }
 
 export const nextTrackAction = function (dispatch: any, trackState: any) {
-	const albumCycle = tracksCycle(trackState.album, trackState.currentTrack.id);
-		
-		getTrackByIdApi(albumCycle).then(async res => {
-			const audio: any = initAudio(res);
-			const duration: any = await getDuration(res.url);
-			trackState.trackData.audio.pause();
-			trackState.trackData.isPlaying && audio.play();
+	const trackId = tracksCycle(trackState.tracksList, trackState.currentTrack.id);
 
-			return dispatch({
-				type: TrackTypes.NEXT_TRACK,
-				payload: {
-					currentTrack: res,
-					trackData: {
-						url: res.url,
-						audio: audio,
-						duration: duration,
-						currentTime: audio.currentTime,
-						timeToEnd: duration - audio.currentTime,
-						isPlaying: trackState.trackData.isPlaying
-					}
+	getTrackByIdApi(trackId).then(async res => {
+		const audio: any = initAudio(res);
+		const duration: any = await getDuration(res.url);
+		trackState.trackData.audio.pause();
+		trackState.trackData.isPlaying && audio.play();
+
+		return dispatch({
+			type: TrackTypes.NEXT_TRACK,
+			payload: {
+				currentTrack: res,
+				trackData: {
+					url: res.url,
+					audio: audio,
+					duration: duration,
+					currentTime: audio.currentTime,
+					timeToEnd: duration - audio.currentTime,
+					isPlaying: trackState.trackData.isPlaying
 				}
-			})
+			}
 		})
+	})
 }
 
 export const previousTrackAction = function (dispatch: any, trackState: any) {
-	const albumReverse = trackState.album.reverse();
-	const albumCycle = tracksCycle(albumReverse, trackState.currentTrack.id);
-		
-		getTrackByIdApi(albumCycle).then(async res => {
-			const audio: any = initAudio(res);
-			const duration: any = await getDuration(res.url);
-			trackState.trackData.audio.pause();
-			trackState.trackData.isPlaying && audio.play();
+	const tracksReverse = trackState.tracksList.reverse();
+	const trackId = tracksCycle(tracksReverse, trackState.currentTrack.id);
 
-			return dispatch({
-				type: TrackTypes.PREV_TRACK,
-				payload: {
-					currentTrack: res,
-					trackData: {
-						url: res.url,
-						audio: audio,
-						duration: duration,
-						currentTime: audio.currentTime,
-						timeToEnd: duration - audio.currentTime,
-						isPlaying: trackState.trackData.isPlaying
-					}
+	getTrackByIdApi(trackId).then(async res => {
+		const audio: any = initAudio(res);
+		const duration: any = await getDuration(res.url);
+		trackState.trackData.audio.pause();
+		trackState.trackData.isPlaying && audio.play();
+
+		return dispatch({
+			type: TrackTypes.PREV_TRACK,
+			payload: {
+				currentTrack: res,
+				trackData: {
+					url: res.url,
+					audio: audio,
+					duration: duration,
+					currentTime: audio.currentTime,
+					timeToEnd: duration - audio.currentTime,
+					isPlaying: trackState.trackData.isPlaying
 				}
-			})
+			}
 		})
+	})
 }
