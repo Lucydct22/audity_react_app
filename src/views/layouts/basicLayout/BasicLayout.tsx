37b84e06d7b77523099231c3386a12759d95d27d
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-23 15:56:23 +0100

    Refactor BasicLayout to use lazy import for components.

--- src/views/layouts/basicLayout/BasicLayout.tsx
+++ src/views/layouts/basicLayout/BasicLayout.tsx
@@ -1,15 +1,15 @@
-import { Suspense } from "react";
+import { Suspense, lazy } from "react";
 import { Outlet } from 'react-router-dom'
 import useWindowSizeReport from "../../../hooks/useWindowSizeReport";
 import { responsiveBreak } from "../../../utils/componentsConstants";
 // Desktop
-import SidebarBComponentDesktop from "../../components/basic/desktop/sidebarBComponentDesktop";
-import PlayerBComponentDesktop from "../../components/basic/desktop/playerBComponentDesktop";
-import TopBarBComponentDesktop from "../../components/basic/desktop/topBarBComponentDesktop";
+const SidebarBComponentDesktop = lazy(() => import('../../components/basic/desktop/sidebarBComponentDesktop'));
+const PlayerBComponentDesktop = lazy(() => import('../../components/basic/desktop/playerBComponentDesktop'));
+const TopBarBComponentDesktop = lazy(() => import('../../components/basic/desktop/topBarBComponentDesktop'));
 // Mobile
-import SidebarBComponentMobile from "../../components/basic/mobile/sidebarBComponentMobile";
-import PlayerBComponentMobile from "../../components/basic/mobile/playerBComponentMobile";
-import TopBarBComponentMobile from "../../components/basic/mobile/topBarBComponentMobile";
+const SidebarBComponentMobile = lazy(() => import('../../components/basic/mobile/sidebarBComponentMobile'));
+const PlayerBComponentMobile = lazy(() => import('../../components/basic/mobile/playerBComponentMobile'));
+const TopBarBComponentMobile = lazy(() => import('../../components/basic/mobile/topBarBComponentMobile'));
 
 const BasicLayout = () => {
   const theme = localStorage.getItem("theme");
@@ -17,27 +17,25 @@ const BasicLayout = () => {
   const [innerWidth] = useWindowSizeReport();
 
   return (
-    <>
-      <Suspense fallback={<></>}>
-        {(innerWidth > responsiveBreak) ? (
-          <>
-            <SidebarBComponentDesktop />
-            <TopBarBComponentDesktop />
-          </>
-        ) : (
-          <>
-            <SidebarBComponentMobile />
-            <TopBarBComponentMobile />
-          </>
-        )}
+    <Suspense fallback={<></>}>
+      {(innerWidth > responsiveBreak) ? (
+        <>
+          <SidebarBComponentDesktop />
+          <TopBarBComponentDesktop />
+        </>
+      ) : (
+        <>
+          <SidebarBComponentMobile />
+          <TopBarBComponentMobile />
+        </>
+      )}
 
-        <div className='main-layout__main'>
-          <Outlet />
-        </div>
+      <div className='main-layout__main'>
+        <Outlet />
+      </div>
 
-        {(innerWidth > responsiveBreak) ? <PlayerBComponentDesktop /> : <PlayerBComponentMobile />}
-      </Suspense>
-    </>
+      {(innerWidth > responsiveBreak) ? <PlayerBComponentDesktop /> : <PlayerBComponentMobile />}
+    </Suspense>
   )
 }
 
