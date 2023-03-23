cd0a6db4a0d5aacbe7e9063a61069579d06322a1
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-23 17:58:40 +0100

    Update trackActions to include functionality for initializing, next, and previous tracks

--- src/reducers/track/trackActions.ts
+++ src/reducers/track/trackActions.ts
@@ -1,36 +1,50 @@
+import { getAlbumsByIdApi } from "../../api/music/albums";
 import { getTrackByIdApi } from "../../api/music/tracks";
+import { getDuration } from "../../utils/tracks/getDuration";
+import { initAudio } from "../../utils/tracks/initAudio";
+import { tracksCycle } from "../../utils/tracks/tracksCycle";
 import * as TrackTypes from './trackTypes'
 
 export const initCurrentTrackAction = function (dispatch: any) {
-	function initAudio(data: any) {
-		const audio = document.createElement("audio");
-		audio.src = data.url;
-		audio.setAttribute("controls", "none");
-		audio.setAttribute("preload", "auto");
-		audio.setAttribute("loop", 'false');
-		audio.setAttribute("muted", 'false');
-		audio.style.display = "none";
-		document.body.appendChild(audio);
-		return audio;
-	}
+	getAlbumsByIdApi('1').then(albumResponse => {
+		const random = Math.floor(Math.random() * albumResponse.tracks.length);
 
-	function getDuration(src: any) {
-		return new Promise(function (resolve) {
-			var audio = new Audio();
-			audio.addEventListener("loadedmetadata", function () {
-				resolve(audio.duration);
-			});
-			audio.src = src;
-		});
-	}
+		getTrackByIdApi(albumResponse.tracks[random]).then(async res => {
+			if (res) {
+				const audio: any = initAudio(res);
+				const duration: any = await getDuration(res.url);
 
-	getTrackByIdApi('1').then(async res => {
-		if (res) {
+				return dispatch({
+					type: TrackTypes.INIT_CURRENT_TRACK,
+					payload: {
+						currentTrack: res,
+						album: albumResponse.tracks,
+						trackData: {
+							url: res.url,
+							audio: audio,
+							duration: duration,
+							currentTime: audio.currentTime,
+							timeToEnd: duration - audio.currentTime,
+							isPlaying: false
+						}
+					}
+				})
+			}
+		})
+	})
+}
+
+export const nextTrackAction = function (dispatch: any, trackState: any) {
+	const albumCycle = tracksCycle(trackState.album, trackState.currentTrack.id);
+		
+		getTrackByIdApi(albumCycle).then(async res => {
 			const audio: any = initAudio(res);
 			const duration: any = await getDuration(res.url);
+			trackState.trackData.audio.pause();
+			trackState.trackData.isPlaying && audio.play();
 
 			return dispatch({
-				type: TrackTypes.INIT_CURRENT_TRACK,
+				type: TrackTypes.NEXT_TRACK,
 				payload: {
 					currentTrack: res,
 					trackData: {
@@ -39,10 +53,36 @@ export const initCurrentTrackAction = function (dispatch: any) {
 						duration: duration,
 						currentTime: audio.currentTime,
 						timeToEnd: duration - audio.currentTime,
-						isPlaying: false
+						isPlaying: trackState.trackData.isPlaying
 					}
 				}
 			})
-		}
-	})
+		})
+}
+
+export const previousTrackAction = function (dispatch: any, trackState: any) {
+	const albumReverse = trackState.album.reverse();
+	const albumCycle = tracksCycle(albumReverse, trackState.currentTrack.id);
+		
+		getTrackByIdApi(albumCycle).then(async res => {
+			const audio: any = initAudio(res);
+			const duration: any = await getDuration(res.url);
+			trackState.trackData.audio.pause();
+			trackState.trackData.isPlaying && audio.play();
+
+			return dispatch({
+				type: TrackTypes.PREV_TRACK,
+				payload: {
+					currentTrack: res,
+					trackData: {
+						url: res.url,
+						audio: audio,
+						duration: duration,
+						currentTime: audio.currentTime,
+						timeToEnd: duration - audio.currentTime,
+						isPlaying: trackState.trackData.isPlaying
+					}
+				}
+			})
+		})
 }
