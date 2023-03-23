e118957af8ef06bf7a3a30e632b986fb487f36d0
Joe Alt <joejoyalt@gmail.com>
2023-03-23 18:11:19 +0100

    feat(responsive): player component bottom

--- src/views/components/basic/mobile/topBarBComponentMobile/TopBarBComponentMobile.jsx
+++ src/views/components/basic/mobile/topBarBComponentMobile/TopBarBComponentMobile.jsx
@@ -20,7 +20,6 @@ const TopBarBComponentMobile = () => {
     let handler = (e) => {
       if (!popperRef.current.contains(e.target)) {
         setPopperOpen(false);
-        console.log(popperRef.current);
       }
     };
 
