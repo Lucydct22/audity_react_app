d3d53dd77285840b6a7cb28c4a4c93497aee716f
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-25 22:42:08 +0100

    Update shuffleArray to accept strings for array elements.

--- src/utils/tracks/shuffleArray.ts
+++ src/utils/tracks/shuffleArray.ts
@@ -1,9 +1,7 @@
-export default function shuffleArray(array: any[]) {
+export default function shuffleArray(array: string[]) {
 	for (let i = array.length - 1; i > 0; i--) {
-		let j = Math.floor(Math.random() * (i + 1));
-		let temp = array[i];
-		array[i] = array[j];
-		array[j] = temp;
+		const j = Math.floor(Math.random() * (i + 1));
+		[array[i], array[j]] = [array[j], array[i]];
 	}
 	return array;
 }
