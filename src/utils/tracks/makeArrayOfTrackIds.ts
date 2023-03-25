786009d07d0dfa289aa2d308deba87d314a2a5cb
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-25 22:41:58 +0100

    Update type of  parameter in  function to  interface.

--- src/utils/tracks/makeArrayOfTrackIds.ts
+++ src/utils/tracks/makeArrayOfTrackIds.ts
@@ -1,6 +1,8 @@
+import { Track } from "../../interfaces/music";
+
 export default function makeArrayOfTrackIds(tracks: any) {
 	let newTracksList: any[] = [];
-	tracks.forEach((track: any) => {
+	tracks.forEach((track: Track) => {
 		newTracksList.push(track.id)
 	});
 	return newTracksList;
