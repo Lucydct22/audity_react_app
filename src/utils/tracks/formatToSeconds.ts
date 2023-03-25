39586854d02a526bcb10c802aef073ffbe63e880
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-25 22:40:44 +0100

    Rename formatToSeconds function to Typescript and update parameter type.

--- src/utils/tracks/formatToSeconds.ts
+++ src/utils/tracks/formatToSeconds.ts
@@ -1,6 +1,6 @@
-export default function formatToSeconds(secs) {
+export default function formatToSeconds(secs: any) {
   secs = parseInt(secs);
-  let ss = secs % 60;
+  let ss: (number | string) = secs % 60;
   const mm = Math.floor(secs / 60.0);
   if (ss < 10) { ss = "0" + ss }
   // if (mm < 10) { mm = "0" + mm }
