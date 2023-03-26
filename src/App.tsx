79e4ab30d865139f534b6607e046cd384e6cec03
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-26 19:43:21 +0200

    feat(serviceWorkers):
    Add service worker functionality to the app, including App Shell-style
    routing, setting up caching for images, and allowing the web app
    to trigger skipWaiting.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,33 @@
 import { RouterProvider } from "react-router-dom";
 import router from './router/router';
+import { Auth0Provider } from '@auth0/auth0-react';
 import { ThemeProvider } from "./context/theme/ThemeContext";
 import { CurrentTracksListProvider } from "./context/currentTracksList/CurrentTracksListProvider";
 import CurrentTrackProvider from "./context/currentTrack/CurrentTrackProvider";
+import { isLocalhost } from './utils/isLocalhost';
 
 const App = () => {
   return (
-    <ThemeProvider>
-      <CurrentTracksListProvider>
-        <CurrentTrackProvider>
-          <RouterProvider
-            router={router}
-            fallbackElement={<></>}
-          />
-        </CurrentTrackProvider>
-      </CurrentTracksListProvider>
-    </ThemeProvider>
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN as string}
+      clientId={
+        isLocalhost
+          ? import.meta.env.VITE_DEVELOPMENT_AUTH0_CLIENT_ID as string
+          : import.meta.env.VITE_PRODUCTION_AUTH0_CLIENT_ID as string
+      }
+      authorizationParams={{ redirect_uri: window.location.origin + '/' }}
+    >
+      <ThemeProvider>
+        <CurrentTracksListProvider>
+          <CurrentTrackProvider>
+            <RouterProvider
+              router={router}
+              fallbackElement={<></>}
+            />
+          </CurrentTrackProvider>
+        </CurrentTracksListProvider>
+      </ThemeProvider>
+    </Auth0Provider>
   );
 }
 
