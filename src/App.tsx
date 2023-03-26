55520330ef5c569813d88303bb3a7d7479b84bd5
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-26 16:18:54 +0200

    Add CurrentTracksListProvider to App component.

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import { TrackProvider } from "./context/currentTrack/TrackContext";
 import { RouterProvider } from "react-router-dom";
 import router from './router/router';
 import { ThemeProvider } from "./context/theme/ThemeContext";
+import { CurrentTracksListProvider } from "./context/currentTracksList/CurrentTracksListProvider";
 
 const App = () => {
   return (
     <ThemeProvider>
-      <TrackProvider>
-        <RouterProvider
-          router={router}
-          fallbackElement={<></>}
-        />
-      </TrackProvider>
+      <CurrentTracksListProvider>
+        <TrackProvider>
+          <RouterProvider
+            router={router}
+            fallbackElement={<></>}
+          />
+        </TrackProvider>
+      </CurrentTracksListProvider>
     </ThemeProvider>
   );
 }
