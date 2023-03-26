4a476efc1e8d3ed2cdd3c3f8c34031333f97f780
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-26 20:10:54 +0200

    remove: serviceworkers

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,9 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import './utils/i18n';
 import './index.scss';
 
-ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement).render(<App />
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <App />
 );
-
-serviceWorkerRegistration.register({
-  onUpdate: (registration: any) => {
-    if (registration && registration.waiting) {
-      registration.waiting.postMessage({ type: "SKIP_WAITING" });
-      registration.update();
-      window.location.reload();
-    }
-  }
-});
