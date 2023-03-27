e490ecf12b8c638ec3a526ae7c06590204ee6ba1
Joe Alt <joejoyalt@gmail.com>
2023-03-27 11:43:22 +0200

    chore(style): text-decoration

--- src/views/components/basic/structureMainBComponent/musicPageComponent/artistComponent/renderArtist/renderArtist.jsx
+++ src/views/components/basic/structureMainBComponent/musicPageComponent/artistComponent/renderArtist/renderArtist.jsx
@@ -4,14 +4,12 @@ import './renderArtist.scss';
 export const RenderArtist = ({ artist }) => {
 
   return (
-    <Link to={`${artist.id}`}>
-      <section className='render-artist' >
-        <div className='render-artist__thumbnail'>
-          <img src={artist.photoUrl} alt={artist.name} />
-        </div>
-        <p className='render-artist__description'>{artist?.name}</p>
-        <p className='render-artist__details'>80,165,532 fans</p>
-      </section>
+    <Link className='render-artist' to={`${artist.id}`}>
+      <div className='render-artist__thumbnail'>
+        <img src={artist.photoUrl} alt={artist.name} />
+      </div>
+      <p className='render-artist__description'>{artist?.name}</p>
+      <p className='render-artist__details'>80,165,532 fans</p>
     </Link>
   )
 }
