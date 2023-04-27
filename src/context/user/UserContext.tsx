4c9f237640a17b0eb4418c894bff32081e8cdfc0
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-27 14:06:21 +0200

    change(userProvider): add any to context type

--- src/context/user/UserContext.tsx
+++ src/context/user/UserContext.tsx
@@ -1,6 +1,22 @@
 import { createContext } from "react";
 import initialUserState from "./initialUserState";
 
-const UserContext = createContext(initialUserState)
+interface InitialUserState {
+	auth0User: object,
+	user: {
+		_id: string,
+		userId: string,
+		name: string,
+		lastname: string,
+		nickname: string,
+		email: string,
+		country: string,
+		language: string,
+		// following: [],
+	},
+	updateUserInfo: () => void
+}
+
+const UserContext = createContext<any>(initialUserState)
 
 export default UserContext
