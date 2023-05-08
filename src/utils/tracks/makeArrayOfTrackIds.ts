23960a970cd877be306a0c99cfb4feee87d2836e
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-05-08 12:08:12 +0200

    change: adapt to backend

--- src/utils/tracks/makeArrayOfTrackIds.ts
+++ src/utils/tracks/makeArrayOfTrackIds.ts
@@ -3,7 +3,7 @@ import { Track } from "interfaces/music";
 export default function makeArrayOfTrackIds(tracks: any) {
 	let newTracklist: any[] = [];
 	tracks.forEach((track: Track) => {
-		newTracklist.push(track.id)
+		newTracklist.push(track._id)
 	});
 	return newTracklist;
 }
