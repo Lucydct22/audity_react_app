88b231c1a6c9e7e79f37adc53ebe450c57e47e48
Iuliia Shikhanova <yshihanova@gmail.com>
2023-05-09 12:20:38 +0100

    create: spinner with style

--- src/views/components/basic/mobile/playerBComponentMobile/PlayerBComponentMobile.tsx
+++ src/views/components/basic/mobile/playerBComponentMobile/PlayerBComponentMobile.tsx
@@ -1,9 +1,7 @@
 import { useContext, useState, useEffect } from 'react';
 import CurrentTrackContext from 'context/currentTrack/CurrentTrackContext';
-// import formatToSeconds from 'utils/tracks/formatToSeconds';
 import { MdSkipPrevious, MdPause, MdPlayArrow, MdSkipNext } from "react-icons/md";
 import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
-// import { IoAddOutline, IoShuffleOutline, IoRepeatOutline, IoVolumeHighOutline, IoVolumeMuteOutline } from "react-icons/io5";
 import './playerBComponentMobile.scss'
 import PlayerTrackDetailsComponentMobile from '../playerTrackDetailsComponentMobile/PlayerTrackDetailsComponentMobile';
 
