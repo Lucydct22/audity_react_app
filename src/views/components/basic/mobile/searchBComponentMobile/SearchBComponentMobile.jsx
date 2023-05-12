febc1d8c8c65d858ea7a88459c11bb431b2d10b5
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-05-12 09:17:45 +0200

    feat: add search api a component

--- src/views/components/basic/mobile/searchBComponentMobile/SearchBComponentMobile.jsx
+++ src/views/components/basic/mobile/searchBComponentMobile/SearchBComponentMobile.jsx
@@ -5,12 +5,15 @@ import { CiSearch } from 'react-icons/ci';
 import { getGenresApi } from 'api/music/genres';
 import { useAuth0 } from '@auth0/auth0-react';
 import { useState, useEffect } from "react";
+import { searchContentApi } from 'api/music/music.api';
 
 export default function SearchBComponentMobile() {
-
   const { t } = useTranslation();
   const [genres, setGenres] = useState(undefined);
   const { user } = useAuth0();
+  const [searchInput, setSearchInput] = useState('')
+  const [content, setContent] = useState('')
+  console.log(content);
 
   useEffect(() => {
     let isMounted = true;
@@ -23,6 +26,19 @@ export default function SearchBComponentMobile() {
     return () => { isMounted = false }
   }, [user])
 
+  const handleSearch = (e) => {
+    setSearchInput(e.target.value)
+  }
+
+  useEffect(() => {
+    let isMounted = true
+    const searchFetch = async () => {
+        const result = await searchContentApi(searchInput)
+        result && isMounted && setContent(result.content)
+    }
+    searchFetch()
+    return () => { isMounted = false }
+  }, [searchInput])
 
   return (
     <div className='mobile-search'>
@@ -30,7 +46,12 @@ export default function SearchBComponentMobile() {
         <span>{t("search_placeholder") || ""}</span>
       </div>
       <div className='mobile-search__input'>
-        <input type="text" className='mobile-search__input--btn' placeholder={t("search_placeholder_mobile") || ""} />
+        <input
+          type="text"
+          className='mobile-search__input--btn'
+          placeholder={t("search_placeholder_mobile") || ""}
+          onChange={(e) => handleSearch(e)}
+        />
         <span><CiSearch size={25} /></span>
       </div>
       <h1 className='mobile-search__genres'>{t("musicpage_genres")}</h1>
