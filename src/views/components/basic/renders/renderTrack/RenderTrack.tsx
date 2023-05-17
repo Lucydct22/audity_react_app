c9d26e3ebc07de3d01a86fde219bcfd417d29218
Joe Alt <joejoyalt@gmail.com>
2023-05-17 13:08:30 +0200

    feat: play pause best 8 songs with style

--- src/views/components/basic/renders/renderTrack/RenderTrack.tsx
+++ src/views/components/basic/renders/renderTrack/RenderTrack.tsx
@@ -1,61 +1,42 @@
-import { useEffect } from "react";
-import { FaPlay } from "react-icons/fa";
 import "./renderTrack.scss";
-import { MdPause, MdPlayArrow, MdArrowBack } from "react-icons/md";
+import { MdPause, MdPlayArrow } from "react-icons/md";
 import { joinArtistsName } from "views/utils/joinArtistsName";
 import CurrentTrackContext from "context/currentTrack/CurrentTrackContext";
-import CurrentTracklistContext from "context/currentTracklist/CurrentTracklistContext";
 import { useContext } from "react";
 import { useTranslation } from "react-i18next";
 
 export default function RenderTrack({ track }: any) {
-  const { listId, selectTrack } = useContext(CurrentTracklistContext);
   const { trackData, selectCurrentTrack, playCurrentTrack, pauseCurrentTrack } =
     useContext(CurrentTrackContext);
   const { name, imageUrl, artists } = track;
   const artistsName = joinArtistsName(artists);
   const { t } = useTranslation();
 
-  const handlePlayClick = () => {
-    selectCurrentTrack(track);
+  const handleClick = () => {
+    if (trackData.url !== track.audioUrl) {
+      selectCurrentTrack(track);
+    } else {
+      trackData.isPlaying ? pauseCurrentTrack() : playCurrentTrack()
+    }
   };
 
   return (
-    <div className="render-track">
+    <div className="render-track" onClick={handleClick}>
       <div className="render-track__thumbnail">
         <div className="render-track__thumbnail--picture">
           <img src={imageUrl} alt={name} />
         </div>
         <div className="render-track__thumbnail--btn">
-          <div className="render-track__thumbnail--btn__play">
-            <button>
-              {listId !== track?._id ? (
-                <div onClick={handlePlayClick}>
-                  <MdPlayArrow size={20} />
-                  <span>{t("playbutton")}</span>
-                </div>
-              ) : (
-                <>
-                  {trackData.isPlaying ? (
-                    <div onClick={pauseCurrentTrack}>
-                      <MdPause size={20} />
-                      <span>{t("pausebutton")}</span>
-                    </div>
-                  ) : (
-                    <div onClick={playCurrentTrack}>
-                      <MdPlayArrow size={20} />
-                      <span>{t("playbutton")}</span>
-                    </div>
-                  )}
-                </>
-              )}
-            </button>
-          </div>
+          <button>
+            {trackData.url === track.audioUrl && trackData.isPlaying ?
+              <MdPause size={20} /> : <MdPlayArrow size={20} />
+            }
+          </button>
         </div>
       </div>
       <div>
         <p className="render-track__name">{name}</p>
-        <p className="render-track__artist">De {artistsName}</p>
+        <p className="render-track__artist">From {artistsName}</p>
       </div>
     </div>
   );
