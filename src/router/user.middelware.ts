1c9b77484ad713b7fc1f48e4787f9bc4a29a1c7a
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-05-19 22:19:18 +0200

    change(MyLibrary): protected route of unidentified users

--- src/router/user.middelware.ts
+++ src/router/user.middelware.ts
@@ -1,6 +1,7 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import UserContext from "context/user/UserContext";
 import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const ProtectedUserSettings = ({ children }: any) => {
   const { isAuthenticated, loginWithRedirect } = useAuth0()
@@ -13,5 +14,20 @@ export const ProtectedUserSettings = ({ children }: any) => {
   }, [dbUser]);
 
     return children;
+
+};
+
+export const ProtectedUser = ({ children }: any) => {
+  const { isAuthenticated } = useAuth0()
+  const { dbUser } = useContext(UserContext)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate('/')
+    }
+  }, [dbUser]);
+
+  return children;
 
 };
