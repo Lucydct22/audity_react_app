ad31483dfd4016f47e090d7bfea5224598a629bf
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-05-19 22:25:50 +0200

    change: make function async

--- src/utils/tracks/tracksCycle.ts
+++ src/utils/tracks/tracksCycle.ts
@@ -1,8 +1,6 @@
-export default function tracksCycle(tracks: any[], id: string) {
+export default async function tracksCycle(tracks: any[], id: string) {
 	let idsArray: any = []
-	tracks.forEach(el => {
-		idsArray.push(el._id)
-	});
+	tracks.forEach(el => idsArray.push(el._id));
 	const index = idsArray.indexOf(id);
 	if (index === -1) return undefined;
 	return tracks[(index + 1) % idsArray.length];
